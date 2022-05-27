2ff0b9510a30f808f5151c5ac0fe24d1d936e8d0
HazemSayad <hazemsayad@gmail.com>
2022-05-27 23:31:41 +0300

    added require for dotenv and node_env condition

--- index.ts
+++ index.ts
@@ -1,39 +1,36 @@
-import DiscordJS, { Intents } from 'discord.js'
-import dotenv from 'dotenv'
-import wokcommands from 'wokcommands'
-import path from 'path'
-import { Player } from 'discord-player'
-import config from './config'
+import DiscordJS, { Intents } from "discord.js";
+import dotenv from "dotenv";
+import wokcommands from "wokcommands";
+import path from "path";
+import { Player } from "discord-player";
+import config from "./config";
 
 //import testSchema from './test-schema'
 
-dotenv.config()
-
 const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-    ]
-})
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
 
 const player = new Player(client, config.opt.discordPlayer);
 
+client.on("ready", () => {
+  new wokcommands(client, {
+    commandDir: path.join(__dirname, "commands"),
+    typeScript: true,
+    mongoUri: process.env.MONGO_URI,
+  });
 
-client.on('ready', () => {
-    new wokcommands(client, {
-        commandDir: path.join(__dirname, 'commands'),
-        typeScript: true,
-        mongoUri: process.env.MONGO_URI,
-    })
-
-    /*     setTimeout(async () => {//make new db collection
+  /*     setTimeout(async () => {//make new db collection
             await new testSchema({
                 message: 'Hello world'
             }).save();
         }, 1000) */
-})
+});
 
-export default player
+export default player;
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
