daf31625b27824a8135d7e2c888289dca4364795
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-06-09 00:46:28 +0300

    Expanded schema fields/paths

--- test-schema.ts
+++ test-schema.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 
-const schema = new mongoose.Schema({
-    message: {
-        type: String,
-        required: true
+const serverSchema = new mongoose.Schema({
+    cmdPrefix: {type: String},
+    messages: {
+        playing: {type: String, required: true},
+        paused: {type: String, required: true},
+        stopped: {type: String, required: true},
+        // expand list of custom messages for each event
     }
 
 })
 
-export default mongoose.model('testing', schema);
+export default mongoose.model('Server', schema); // this will look in servers collection, plural.
